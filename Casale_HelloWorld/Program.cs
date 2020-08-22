239e7515974ac44d4608810bcd13942b0c30aa56
Lauren Casale <laurencasale2000@hotmail.com>
2020-08-22 14:40:44 -0400

    Adding HelloWorld app properly

---
+++ Casale_HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            int num1 = 3;
+            int num2 = 4;
+            int i = 0;
+
+            string stringNum = "12";
+
+            //math
+            int number = num1 * num2;
+
+            //loop
+            for (i = 0; i < number; ++i)
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            //writing
+            Console.WriteLine("My name is Lauren Casale.");
+            Console.WriteLine($"My favorite number is {number + "."}");
+
+        }
+    }
+}
--- Casale_HelloWorld/Properties/AssemblyInfo.cs
+++ Casale_HelloWorld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("favNumColor")]
+[assembly: AssemblyTitle("Casale_HelloWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("favNumColor")]
+[assembly: AssemblyProduct("Casale_HelloWorld")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f34e8df-9568-4391-83f4-daf99cd8dd39")]
+[assembly: Guid("f9558ad5-6183-4b99-aeb9-7da4d02d9c9c")]
 
 // Version information for an assembly consists of the following four values:
 //
