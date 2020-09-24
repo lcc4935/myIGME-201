e20d72163bcc0754be00e921c8924671ea77ee35
Lauren Casale <laurencasale2000@hotmail.com>
2020-09-23 23:38:50 -0400

    PE12 Defining Class Members

---
+++ Casale_PE12_DefiningClassMembers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casale_PE12_DefiningClassMembers
+{
+    public class MyClass
+    {
+        private string myString;
+        public string MyString
+        {
+            set
+            {
+            }
+        }
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }
+
+    public class MyDerivedClass: MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + "output from the derived class";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass myDClass = new MyDerivedClass();
+            string sString = myDClass.GetString();
+        }
+    }
+}
