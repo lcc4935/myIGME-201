c5d781330ba744a4fbfcc04006bf8534ff90b343
Lauren Casale <laurencasale2000@hotmail.com>
2020-10-14 21:14:34 -0400

    Unit Test 2

---
+++ Casale_UnitTest2_Q4-7/Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casale_UnitTest2_Q4_7
+{
+    //Lauren Casale Unit Test 2; Q 4-7
+
+    //Abstract Class
+    //Phone
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string PhoneNumber;
+        public string address;
+
+        public abstract void Connect();
+        public abstract void Disconnect();
+    }
+
+
+    //Interface
+    //PhoneInterface
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }
+
+
+    //Class
+    //RotaryPhone
+    public class RotaryPhone: Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+        public void MakeCall()
+        {
+
+        }
+        public void HangUp()
+        {
+
+        }
+        public override void Connect()
+        {
+
+        }
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+
+    //Class
+    //PushButtonPhone
+    public class PushButtonPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+        public void MakeCall()
+        {
+
+        }
+        public void HangUp()
+        {
+
+        }
+        public override void Connect()
+        {
+
+        }
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    //Class
+    //Tardis
+
+    public class Tardis: RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get
+            {
+                return whichDrWho;
+            }
+        }
+        public string FemaleSideKick
+        {
+            get
+            {
+                return femaleSideKick;
+            }
+        }
+        public void TimeTravel()
+        {
+
+        }
+
+        public static bool operator < (Tardis dr1, Tardis dr2)
+        {
+            if (dr2.whichDrWho == 10)
+            {
+                return true;
+            }
+            if (dr1.whichDrWho == 10)
+            {
+                return false;
+            }
+            if (dr1.whichDrWho == dr2.whichDrWho)
+            {
+                return false;
+            }
+            
+            return (dr1.whichDrWho < dr2.whichDrWho);
+                
+        }
+
+        public static bool operator > (Tardis dr1, Tardis dr2)
+        {
+            if (dr1.whichDrWho == 10)
+            {
+                return true;
+            }
+            if (dr2.whichDrWho == 10)
+            {
+                return false;
+            }
+            if (dr1.whichDrWho == dr2.whichDrWho)
+            {
+                return false;
+            }
+
+            return (dr1.whichDrWho > dr2.whichDrWho);
+        }
+
+        public static bool operator <= (Tardis dr1, Tardis dr2)
+        {
+            if (dr2.whichDrWho == 10)
+            {
+                return true;
+            }
+            if (dr1.whichDrWho == 10)
+            {
+                return false;
+            }
+
+            return (dr1.whichDrWho <= dr2.whichDrWho);
+        }
+
+        public static bool operator >= (Tardis dr1, Tardis dr2)
+        {
+            if (dr1.whichDrWho == 10)
+            {
+                return true;
+            }
+            if (dr2.whichDrWho == 10)
+            {
+                return false;
+            }
+
+            return (dr1.whichDrWho >= dr2.whichDrWho);
+        }
+
+        public static bool operator == (Tardis dr1, Tardis dr2)
+        {
+            return (dr1.whichDrWho == dr2.whichDrWho);
+        }
+
+        public static bool operator != (Tardis dr1, Tardis dr2)
+        {
+            return (dr1.whichDrWho != dr2.whichDrWho);
+        }
+    }
+
+
+    //Class
+    //PhoneBooth
+    public class PhoneBooth: PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+
+        }
+        public void CloseDoor()
+        {
+
+        }
+    }
+
+
+
+    //Class
+    //Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tardis tardis = new Tardis();
+            PhoneBooth phoneBooth = new PhoneBooth();
+
+            UsePhone(tardis);
+            UsePhone(phoneBooth);
+
+        }
+
+        static void UsePhone(object obj)
+        {
+            PhoneInterface phoneInterface;
+            phoneInterface = (PhoneInterface)obj;
+            phoneInterface.MakeCall();
+
+            if (phoneInterface.GetType() == typeof(Tardis))
+            {
+                Tardis tardis;
+                tardis = (Tardis)obj;
+                tardis.TimeTravel();
+            }
+            if (phoneInterface.GetType() == typeof(PhoneBooth))
+            {
+                PhoneBooth phoneBooth;
+                phoneBooth = (PhoneBooth)obj;
+                phoneBooth.OpenDoor();
+            }
+        }
+    }
+
+}
---
+++ Casale_UnitTest2_Q9-10/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casale_UnitTest2_Q9_10
+{
+    //Lauren Casale Unit Test 2; Q 9-10
+
+
+    //Abstract Class
+    //Hockey
+    public abstract class Hockey
+    {
+        public abstract void Play();
+        public virtual void iceTime()
+        {
+
+        }
+    }
+
+
+    //Interface
+    //ISkating
+    public interface ISkating
+    {
+        void Skate();
+    }
+
+
+    //Class
+    //Skating
+    public class Skating: Hockey, ISkating
+    {
+        public override void Play()
+        {
+
+        }
+        public void Skate()
+        {
+            Console.WriteLine("Skating");
+        }
+    }
+
+
+    //Interface
+    //IPuckHandling
+    public interface IPuckHandling
+    {
+        void Puck();
+    }
+
+
+    //Class
+    //PuckHandling
+    public class PuckHandling: Hockey, IPuckHandling
+    {
+        public override void Play()
+        {
+
+        }
+        public void Puck()
+        {
+            Console.WriteLine("Puck usage");
+        }
+    }
+
+
+    //Class
+    //Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Skating skating = new Skating();
+            PuckHandling puckHandling = new PuckHandling();
+
+            MyMethod(skating);
+            MyMethod(puckHandling);
+
+        }
+
+        public static void MyMethod(object obj)
+        {
+
+            IPuckHandling iPuckHandling;
+            iPuckHandling = (IPuckHandling)obj;
+            iPuckHandling.Puck();
+
+            ISkating iSkating;
+            iSkating = (ISkating)obj;
+            iSkating.Skate();
+
+        }
+
+
+    }
+}
