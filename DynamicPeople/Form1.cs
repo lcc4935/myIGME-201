f741513fd4064381235f07abed575d767d6f8ac6
Lauren Casale <laurencasale2000@hotmail.com>
2020-11-11 15:57:09 -0500

    Dynamic People PE 25

---
+++ DynamicPeople/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PeopleLib;
+using PeopleAppGlobals;
+using CourseLib;
+using EditPerson;
+
+namespace DynamicPeople
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Globals.AddPeopleSampleData();
+
+            this.teacherButton.Click += new EventHandler(TeacherButton__Click);
+            this.studentButton.Click += new EventHandler(StudentButton__Click);
+
+            this.panel1.Visible = false;
+
+        }
+
+        private void TeacherButton__Click(object sender, EventArgs e)
+        {
+            this.flowLayoutPanel1.Controls.Clear();
+            foreach(KeyValuePair<string,Person> keyValuePair in Globals.people.sortedList)
+            {
+                if (keyValuePair.Value.GetType() == typeof(Teacher))
+                {
+                    AddPanel(keyValuePair.Value);
+                }
+            }
+
+            teacherButton.Text = this.flowLayoutPanel1.Controls.Count.ToString();
+        }
+
+        private void StudentButton__Click(object sender, EventArgs e)
+        {
+            this.flowLayoutPanel1.Controls.Clear();
+            foreach (KeyValuePair<string, Person> keyValuePair in Globals.people.sortedList)
+            {
+                if (keyValuePair.Value.GetType() == typeof(Student))
+                {
+                    AddPanel(keyValuePair.Value);
+                }
+            }
+
+            studentButton.Text = this.flowLayoutPanel1.Controls.Count.ToString();
+        }
+
+        private void AddPanel(Person person)
+        {
+            Panel panel1 = new System.Windows.Forms.Panel();
+
+            AddPersonToPanel(ref panel1, person);
+
+            this.flowLayoutPanel1.Controls.Add(panel1);
+            this.flowLayoutPanel1.Controls.SetChildIndex(panel1, flowLayoutPanel1.Controls.Count);
+        }
+
+        private void AddPersonToPanel(ref Panel panel1, Person person)
+        {
+            
+            ToolStrip toolStrip1 = new System.Windows.Forms.ToolStrip();
+            ToolStripButton toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ToolStripLabel toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            Label emailLabel = new System.Windows.Forms.Label();
+            GroupBox photoGroupBox = new System.Windows.Forms.GroupBox();
+            PictureBox photoPictureBox = new System.Windows.Forms.PictureBox();
+
+            //
+            // panel1
+            // 
+            panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            panel1.Controls.Add(emailLabel);
+            panel1.Controls.Add(photoGroupBox);
+            panel1.Controls.Add(toolStrip1);
+            panel1.Location = new System.Drawing.Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(193, 25);
+            panel1.TabIndex = 0;
+            panel1.Tag = person;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            toolStripButton1,
+            toolStripLabel1});
+            toolStrip1.Location = new System.Drawing.Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            toolStrip1.Size = new System.Drawing.Size(189, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = global::DynamicPeople.Properties.Resources.plus;
+            toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.Click += new EventHandler(ToolStripButton1__Click);
+            toolStripButton1.Tag = panel1;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.AutoSize = false;
+            this.toolStripLabel1.IsLink = true;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripLabel1.Size = new System.Drawing.Size(140, 22);
+            this.toolStripLabel1.Text = person.name;
+            this.toolStripLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            toolStripLabel1.Click += new EventHandler(ToolStripLabel1__Click);
+            toolStripLabel1.Tag = panel1;
+            // 
+            // emailLabel
+            // 
+            emailLabel.Location = new System.Drawing.Point(19, 49);
+            emailLabel.Name = "emailLabel";
+            emailLabel.Size = new System.Drawing.Size(167, 23);
+            emailLabel.TabIndex = 1;
+            emailLabel.Text = person.email;
+            // 
+            // photoGroupBox
+            // 
+            photoGroupBox.Controls.Add(photoPictureBox);
+            photoGroupBox.Location = new System.Drawing.Point(22, 75);
+            photoGroupBox.Name = "photoGroupBox";
+            photoGroupBox.Size = new System.Drawing.Size(134, 121);
+            photoGroupBox.TabIndex = 52;
+            photoGroupBox.TabStop = false;
+            photoGroupBox.Text = "Photo";
+            // 
+            // photoPictureBox
+            // 
+           photoPictureBox.BackColor = System.Drawing.Color.LightGray;
+           photoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+           photoPictureBox.Location = new System.Drawing.Point(3, 16);
+           photoPictureBox.Name = "photoPictureBox";
+           photoPictureBox.Size = new System.Drawing.Size(128, 102);
+           photoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+           photoPictureBox.TabIndex = 0;
+           photoPictureBox.TabStop = false;
+            photoPictureBox.ImageLocation = person.photoPath;
+        }
+
+        private void ToolStripButton1__Click(object sender, EventArgs e)
+        {
+            ToolStripButton tbs = (ToolStripButton)sender;
+            Panel p = (Panel)tbs.Tag;
+
+            if (tbs.Checked)
+            {
+                tbs.Image = global::DynamicPeople.Properties.Resources.plus;
+                p.Size = new System.Drawing.Size(193, 25);
+                tbs.Checked = false;
+            }
+            else
+            {
+                tbs.Image = global::DynamicPeople.Properties.Resources.minus;
+                p.Size = new System.Drawing.Size(193, 211);
+                tbs.Checked = true;
+            }
+
+            p.Refresh();
+        }
+
+        private void ToolStripLabel1__Click(object sender, EventArgs e)
+        {
+            ToolStripLabel tsl = (ToolStripLabel)sender;
+            Panel p = (Panel)tsl.Tag;
+
+            PersonEditForm pef = new PersonEditForm((Person)p.Tag, this);
+            pef.Visible = false;
+
+            pef.ShowDialog();
+
+            Person person = pef.formPerson;
+
+            p.Controls.Clear();
+
+            AddPersonToPanel(ref p, person);
+
+            p.Refresh();
+        }
+
+
+    }
+}
